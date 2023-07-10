33d740ec88f5d3098a17081d1ff3e8559337fc45
leont <leon.traykov@gmail.com>
2023-07-10 13:11:38 -0400

    db bug fixed

--- db/migrate/20230706222303_remove_fields_from_messages.rb
+++
@@ -1,6 +0,0 @@
-class RemoveFieldsFromMessages < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :messages, :sender_id, :integer
-    remove_column :messages, :recipient_id, :integer
-  end
-end
