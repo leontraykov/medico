a057bdf0f9a52707a888d5b2de606634091c168c
leont <leon.traykov@gmail.com>
2023-08-22 23:25:41 -0400

    predeploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, "git@github.com:leontraykov/medico.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, 'main'
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
