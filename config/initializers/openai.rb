ba66f5b34c063deaf3f884129f0e32ed65d9a7c4
leont <leon.traykov@gmail.com>
2023-08-23 00:38:31 -0400

    predeploy

--- config/initializers/openai.rb
+++
@@ -1,4 +0,0 @@
-OpenAI.configure do |config|
-  config.access_token =  Rails.application.credentials.openai.access_token
-  config.organization_id = Rails.application.credentials.openai.organization_id
-end
