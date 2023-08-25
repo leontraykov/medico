d03d78d44183497bba02d0f83040cc06ff3dff7e
leont <leon.traykov@gmail.com>
2023-08-24 21:26:17 -0400

    full I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,23 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
+
+  around_action :switch_locale
+
+  private
+
+  def switch_locale(&action)
+    locale = locale_from_url || I18n.default_locale
+    I18n.with_locale locale, &action
+  end
+
+  def locale_from_url
+    locale = params[:locale]
+
+    return locale if I18n.available_locales.map(&:to_s).include?(locale)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'pages/home'
-  devise_for :users
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    get 'pages/home'
+    devise_for :users
+    resources :users
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "pages#home"
-  # root 'chat_rooms#index'
-  resources :chat_rooms, only: %i[index new create show] do
-    resources :messages, only: [:create]
+    # Defines the root path route ("/")
+    root "pages#home"
+    # root 'chat_rooms#index'
+    resources :chat_rooms, only: %i[index new create show] do
+      resources :messages, only: [:create]
+    end
+
+    resources :questions, only: [:index, :create]
   end
-
-  resources :questions, only: [:index, :create]
 end
